b570c68c28fa61224a74e53eee714a5cd60ee148
Ryan <ryanpauley@gmail.com>
2015-01-12 19:33:53 +1100

    CORS

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module Yaps
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
