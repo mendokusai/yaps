4c00d54e266d600048ceef305f01a5771c341aaf
Ryan <ryanpauley@gmail.com>
2015-01-13 12:23:56 +1100

    RAILS AND NODE = LOVE

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.socket_url = "localhost:3001"
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
