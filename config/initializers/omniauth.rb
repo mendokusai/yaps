c29912a641240580bf502d42c196f7c881856394
Joel Hampton <joelhampton@gmail.com>
2015-01-15 09:59:31 +1100

    Updates to Reddit API requests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :reddit, Rails.application.secrets.reddit_api_key, Rails.application.secrets.reddit_api_secret, duration: "permanent", scope: "identity,history,mysubreddits", provider_ignores_state: true
+  provider :reddit, Rails.application.secrets.reddit_api_key, Rails.application.secrets.reddit_api_secret, duration: "permanent", scope: "identity,history,mysubreddits,read", provider_ignores_state: true
 end
 
 
